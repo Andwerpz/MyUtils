55b7fc1af3252d02af55ac03b70b18b1f1640c6a
Andrew Li <andwerpooni@gmail.com>
2023-03-17 21:43:22 -0500

    initial commit

---
+++ MyUtils/src/math/Vec4.java
@@ -0,0 +1,42 @@
+package math;
+
+import java.nio.FloatBuffer;
+
+public class Vec4 {
+
+	public float x, y, z, w;
+
+	public Vec4(float x, float y, float z, float w) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.w = w;
+	}
+
+	public Vec4(Vec4 v) {
+		this.x = v.x;
+		this.y = v.y;
+		this.z = v.z;
+		this.w = v.w;
+	}
+
+	public Vec4(Vec3 v, float w) {
+		this.x = v.x;
+		this.y = v.y;
+		this.z = v.z;
+		this.w = w;
+	}
+
+	public Vec4(float a) {
+		this.x = a;
+		this.y = a;
+		this.z = a;
+		this.w = a;
+	}
+
+	@Override
+	public String toString() {
+		return this.x + ", " + this.y + ", " + this.z + ", " + this.w;
+	}
+
+}
